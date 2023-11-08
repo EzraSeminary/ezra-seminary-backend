42f750b8b70335124458db377577348a5532e439
Besufekad-HAZ <besurye@gmail.com>
2023-11-08 13:42:25 +0300

    Fix multer and Deploye

--- Server.js
+++ Server.js
@@ -14,9 +14,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/', require('./routes/root'))
+
 app.use('/devotion', devotionRoutes);
 
 app.use("/images", express.static("public/images"));
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
