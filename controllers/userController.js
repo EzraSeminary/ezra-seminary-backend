81afcb275a1d886ec904ee2a7529c694b548ba0f
Besufekad-HAZ <besurye@gmail.com>
2023-12-01 12:42:17 +0300

    Add user controller and instal other neccesary dependencies (JWT), email validator etc...

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+// Create JWT
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
+// Login Controller
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    // create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+// Signup Controller
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    // create token
+    const token = createToken(user.id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  loginUser,
+  signupUser,
+};
