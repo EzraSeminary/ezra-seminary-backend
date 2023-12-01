69d0073b18521f2d5205d44b17d126b3b12db790
Besufekad-HAZ <besurye@gmail.com>
2023-12-01 12:58:00 +0300

    Update devotion routes.js to check if the JWT token is valid

--- routes/devotionRoutes.js
+++ routes/devotionRoutes.js
@@ -8,8 +8,11 @@ const express = require('express');
 const router = express.Router();
 const devotionController = require('../controllers/devotionController');
 const upload = require('../middleware/upload');
+const verifyJWT = require('../middleware/requireAuth');
 
 const { createDevotion, getDevotions, deleteDevotion } = devotionController;
+
+router.use(verifyJWT);
 
 router.route('/create')
   .post(upload.single('image'), createDevotion);
