799fbb513bf9aa742ae662d2c619df15f49495d3
Besufekad-HAZ <besurye@gmail.com>
2023-12-08 11:29:16 +0300

    Add new port in CORS again

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "chrome-extension://gmmkjpcadciiokjpikmkkmapphbmdjok",
   "http://localhost:5173",
   "http://127.0.0.1:5175",
+  "http://localhost:5175",
 ];
 
 // const allowedOrigins = '*';
