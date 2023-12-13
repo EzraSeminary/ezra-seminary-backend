d6a1bcab17cbc969e5684e1478755198abfce880
Besufekad-HAZ <besurye@gmail.com>
2023-12-13 18:30:28 +0300

    Update the model and add First name and last name in the mongoose schema

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true }, // email must be unique
   password: { type: String, required: true },
   role: {
