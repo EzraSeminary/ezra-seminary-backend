8353fb390ded3997f4d15bb9f8822b9cd188bf3a
Besufekad-HAZ <besurye@gmail.com>
2023-12-27 13:00:02 +0300

    try serving the front end build code to the backend server

--- Server.js
+++ Server.js
@@ -38,15 +38,10 @@ app.use("/quiz", quizController);
 
 // app.use("/images", express.static("public/images"));
 
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ message: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
 // listen for requests
