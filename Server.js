5284ac75e32a4a9e85b19d1faf6dcf7e7550d949
Besufekad-HAZ <besurye@gmail.com>
2023-12-27 13:10:21 +0300

    rollback to the previous state

--- Server.js
+++ Server.js
@@ -38,10 +38,15 @@ app.use("/quiz", quizController);
 
 // app.use("/images", express.static("public/images"));
 
-app.use(express.static(path.join(__dirname, "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 // listen for requests
