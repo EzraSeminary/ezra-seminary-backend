5ea52bd487c5ac11416ce64bfa1bacf5fa9bb698
Besufekad-HAZ <besurye@gmail.com>
2023-12-28 21:10:32 +0300

    Add Netlify site for the front end

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -6,6 +6,7 @@ const allowedOrigins = [
   "http://localhost:5175",
   "https://ezra-test-frontend.onrender.com",
   "https://ezra-test-frontend.onrender.com/",
+  "https://sage-cassata-4d464e.netlify.app'",
 ];
 
 // // const allowedOrigins = "*";
