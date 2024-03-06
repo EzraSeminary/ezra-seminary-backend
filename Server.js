9d145f24ae71c3a0064bff0660524b029a920cc9
Besufekad-HAZ <besurye@gmail.com>
2024-03-07 00:16:39 +0300

    Fix Helmet errors

--- Server.js
+++ Server.js
@@ -15,7 +15,16 @@ const courseController = require("./controllers/courseController");
 const quizController = require("./controllers/quizController");
 const requireAuth = require("./middleware/requireAuth");
 
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "img-src": ["'self'", "ezra-seminary.mybese.tech", "data:"],
+      },
+    },
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 
 connectDb();
