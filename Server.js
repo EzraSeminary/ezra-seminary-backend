ee2bb89328cb3a6b58b5e1f0095e62ce80d3da2b
Besufekad-HAZ <besurye@gmail.com>
2024-03-11 18:32:07 +0300

    Fix Click jacking x frame and other security issues

--- Server.js
+++ Server.js
@@ -1,7 +1,6 @@
 // app.js
 
 const express = require("express");
-const helmet = require("helmet");
 
 const app = express();
 const connectDb = require("./config/connectDb");
@@ -15,23 +14,17 @@ const courseController = require("./controllers/courseController");
 const quizController = require("./controllers/quizController");
 const requireAuth = require("./middleware/requireAuth");
 
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        "img-src": [
-          "'self'",
-          "ezra-seminary.mybese.tech",
-          "localhost:5173",
-          "localhost:5174",
-          "data:",
-        ],
-      },
-    },
-  })
-);
 app.use(express.json({ limit: "50mb" }));
+
+app.use((req, res, next) => {
+  res.setHeader("X-Frame-Options", "DENY");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "frame-ancestors 'none'");
+  next();
+});
 
 connectDb();
 
