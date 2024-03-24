9edba86f112bf21ed0ac8c1a118610f0cfe8e51d
Besufekad-HAZ <besurye@gmail.com>
2024-03-24 21:19:43 +0300

    Implement Devotion image download request to be handled

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,28 +1,26 @@
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/images');
+    cb(null, path.join(__dirname, "../public", "images"));
   },
   filename: (req, file, cb) => {
-    const timestamp = new Date().toISOString().replace(/:/g, '-');
+    const timestamp = new Date().toISOString().replace(/:/g, "-");
     cb(null, `${timestamp}-${file.originalname}`);
   },
 });
-
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) {
+  if (file.mimetype.startsWith("image/")) {
     cb(null, true);
   } else {
-    cb(new Error('Only image files are allowed'), false);
+    cb(new Error("Only image files are allowed"), false);
   }
 };
 
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 10 * 1024 * 1024 } // 10 MB limit
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB limit
 });
 
 module.exports = upload;
-
