bbdb314d22f910082941e61911ef50e3e7fd5421
Besufekad-HAZ <besurye@gmail.com>
2024-04-22 19:50:58 +0300

    Add contact model

---
+++ models/Contact.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+module.exports = Contact;
