c09ee9223e58ff74135450a1a070904f74d9bce0
Besufekad-HAZ <besurye@gmail.com>
2024-05-02 12:40:32 +0300

    Update analytics controllers to track users and courses within the last month

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,22 +1,19 @@
 // controllers/analyticsController.js
-const User = require("../models/User");
-const Course = require("../models/Course");
-
 const getAnalytics = async (req, res) => {
   try {
     const newUsers = await User.countDocuments({
-      createdAt: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
+      createdAt: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) }, // Count users created in the last 30 days
     });
     const totalUsers = await User.countDocuments();
     const newCourses = await Course.countDocuments({
-      createdAt: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
+      createdAt: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) }, // Count courses created in the last 30 days
     });
     const totalCourses = await Course.countDocuments();
     const accountsReached = await User.countDocuments({
       lastLogin: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
     });
     const usersLeft = await User.countDocuments({
-      lastLogin: { $lt: new Date(Date.now() - 60 * 24 * 60 * 60 * 1000) },
+      lastLogin: { $lt: new Date(Date.now() - 60 * 24 * 60 * 60 * 1000) }, // Count users who haven't logged in for the last 2 months
     });
 
     const analyticsData = {
