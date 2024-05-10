ed2ace18093520007eb6880753ed0c17f475ef40
Besufekad-HAZ <besurye@gmail.com>
2024-05-10 18:03:53 +0300

    Remove unnecessary req and res from getAnalytics function into a separate route handler and not have it send the response directly

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -5,6 +5,14 @@ const router = express.Router();
 const analyticsController = require("../controllers/analyticsController");
 const requireAuth = require("../middleware/requireAuth");
 
-router.get("/", requireAuth, analyticsController.getAnalytics);
+router.get("/", requireAuth, async (req, res) => {
+  try {
+    const analyticsData = await analyticsController.getAnalytics();
+    res.json(analyticsData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 module.exports = router;
