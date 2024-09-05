36e24b85772a8ae0034689ba846673975c20e8d3
Besufekad-HAZ <besurye@gmail.com>
2024-09-05 10:39:21 +0300

    Update Coursecontrollers with cloudinary upload links

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,40 +1,47 @@
-const multer = require("multer");
 const express = require("express");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const Course = require("../models/Course");
-const courseController = require("express").Router();
+const courseController = express.Router();
 const verifyJWT = require("../middleware/requireAuth");
 const path = require("path");
 
-// image upload
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    const timestamp = new Date().toISOString().replace(/:/g, "-");
-    const fileExtension = path.extname(file.originalname); // Extract file extension
-    // Extract the base name without target location path
-    const baseFileName = path.basename(file.originalname, fileExtension);
-    // Prepend timestamp and append the original file extension
-    cb(null, `${timestamp}-${baseFileName}${fileExtension}`);
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: "dy233t3yl",
+  api_key: "737799364788289",
+  api_secret: "vnIYVSSiA5D3PVtRL22gt8i9zjE",
+});
+
+// Configure Cloudinary storage
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "Courses",
+    allowed_formats: ["jpg", "png", "jpeg", "gif", "mp3", "wav"],
+    resource_type: "auto",
+    public_id: (req, file) => {
+      const originalName = path.parse(file.originalname).name;
+      return originalName; // Return the original filename without extension
+    },
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype.startsWith("image/") ||
-    file.mimetype.startsWith("audio/")
-  ) {
-    cb(null, true); // Accept file
-  } else {
-    cb(new Error("Only image & audio files are allowed"), false); // Reject file
-  }
-};
-
+// Configure Multer
 const upload = multer({
   storage: storage,
-  fileFilter: fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
+  limits: { fileSize: 15 * 1024 * 1024 }, // 15 MB limit
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype.startsWith("image/") ||
+      file.mimetype.startsWith("audio/")
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only image & audio files are allowed"), false);
+    }
+  },
 });
 
 // get all courses
@@ -68,14 +75,12 @@ courseController.get("/getchapter/:courseId/:chapterId", async (req, res) => {
     const courseId = req.params.courseId;
     const chapterId = req.params.chapterId;
 
-    // First, we find the course by id
     const course = await Course.findById(courseId);
 
     if (!course) {
       return res.status(404).json({ message: "Course not found" });
     }
 
-    // Next, we find the chapter by id within the course
     const chapter = course.chapters.find(
       (chap) => chap._id.toString() === chapterId
     );
@@ -91,95 +96,68 @@ courseController.get("/getchapter/:courseId/:chapterId", async (req, res) => {
   }
 });
 
-//create course
+// create course
 courseController.post("/create", upload.any(), async (req, res) => {
   const { title, description, published } = req.body;
   const files = req.files || [];
 
-  // Create a map for quick file lookups based on the multipart field name
-  const fileMap =
-    req.files && req.files.length > 0
-      ? req.files.reduce((map, file) => {
-          map[file.fieldname] = file.filename;
-          return map;
-        }, {})
-      : {};
-
   let chapters;
   try {
-    // Ensure chapters is being sent as a string, and then parse it.
-    // If chapters are being sent as object, we need to handle parsing manually.
     if (typeof req.body.chapters === "string") {
       chapters = JSON.parse(req.body.chapters);
     } else {
-      // Handle the case where chapters is already an object, probably due to 'multipart/form-data' being used.
-      chapters = req.body.chapters; // Potentially already parsed by middleware
+      chapters = req.body.chapters;
     }
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
 
-  // Functionality moved inside the POST route
-  const updatedChapters = chapters.map((chapter, chapterIndex) => ({
-    ...chapter,
-    slides: chapter.slides.map((slide, slideIndex) => ({
-      ...slide,
-      elements: slide.elements.map((element) => {
-        // If it's an img type element
-        if (element.type === "img") {
-          const fieldName = `chapter_${chapterIndex}_slide_${slideIndex}_image`;
-          const file = req.files.find((f) => f.fieldname === fieldName);
-          if (file) {
-            return {
-              ...element,
-              value: file.filename, // Correctly reference the filename here
-            };
-          }
-        }
+  const updatedChapters = await Promise.all(
+    chapters.map(async (chapter, chapterIndex) => ({
+      ...chapter,
+      slides: await Promise.all(
+        chapter.slides.map(async (slide, slideIndex) => ({
+          ...slide,
+          elements: await Promise.all(
+            slide.elements.map(async (element) => {
+              if (
+                element.type === "img" ||
+                element.type === "audio" ||
+                (element.type === "mix" && element.value.file)
+              ) {
+                const fieldName = `chapter_${chapterIndex}_slide_${slideIndex}_${
+                  element.type === "mix" ? "mix_file" : element.type
+                }`;
+                const file = files.find((f) => f.fieldname === fieldName);
+                if (file) {
+                  return {
+                    ...element,
+                    value:
+                      element.type === "mix"
+                        ? {
+                            ...element.value,
+                            file: file.path,
+                          }
+                        : file.path,
+                  };
+                }
+              }
+              return element;
+            })
+          ),
+        }))
+      ),
+    }))
+  );
 
-        // If it's a mix type element
-        if (element.type === "mix") {
-          const fieldName = `chapter_${chapterIndex}_slide_${slideIndex}_mix_file`;
-          const file = req.files.find((f) => f.fieldname === fieldName);
-          if (file) {
-            return {
-              ...element,
-              value: {
-                text1: element.value.text1,
-                file: file.filename, // Correctly reference the filename here
-                text2: element.value.text2,
-              },
-            };
-          }
-        }
-
-        // If it's an audio type element
-        if (element.type === "audio") {
-          const fieldName = `chapter_${chapterIndex}_slide_${slideIndex}_audio`;
-          const file = req.files.find((f) => f.fieldname === fieldName);
-          if (file) {
-            return {
-              ...element,
-              value: file.filename, // Correctly reference the filename here
-            };
-          }
-        }
-
-        return element;
-      }),
-    })),
-  }));
-  console.log("Request Files:", req.files); // Log uploaded file details
-
-  // Use files variable instead of req.files directly
   const imageFile = files.find((file) => file.fieldname === "image");
-  const imageFileName = imageFile ? imageFile.filename : "";
+  const imageUrl = imageFile ? imageFile.path : "";
 
   try {
     const newCourse = new Course({
       title,
       description,
-      image: imageFileName,
+      image: imageUrl,
       chapters: updatedChapters,
       published,
     });
@@ -199,20 +177,10 @@ courseController.put("/update/:id", upload.any(), async (req, res) => {
     const { title, description, published } = req.body;
     const files = req.files || [];
 
-    // Start by finding the existing course
     const course = await Course.findById(courseId);
     if (!course) {
       return res.status(404).json({ message: "Course not found" });
     }
-
-    // If there are files being uploaded, handle the file upload similarly to the create
-    const fileMap =
-      req.files && req.files.length > 0
-        ? req.files.reduce((map, file) => {
-            map[file.fieldname] = file.filename;
-            return map;
-          }, {})
-        : {};
 
     let chapters;
     try {
@@ -225,55 +193,55 @@ courseController.put("/update/:id", upload.any(), async (req, res) => {
       return res.status(400).json({ message: error.message });
     }
 
-    // Process the chapters as done in the create controller
-    const updatedChapters = chapters.map((chapter, chapterIndex) => ({
-      ...chapter,
-      slides: chapter.slides.map((slide, slideIndex) => ({
-        ...slide,
-        elements: slide.elements.map((element) => {
-          // If it's an img type element
-          if (element.type === "img") {
-            const fieldName = `chapter_${chapterIndex}_slide_${slideIndex}_image`;
-            const file = req.files.find((f) => f.fieldname === fieldName);
-            if (file) {
-              return {
-                ...element,
-                value: file.filename,
-              };
-            }
-          }
+    const updatedChapters = await Promise.all(
+      chapters.map(async (chapter, chapterIndex) => ({
+        ...chapter,
+        slides: await Promise.all(
+          chapter.slides.map(async (slide, slideIndex) => ({
+            ...slide,
+            elements: await Promise.all(
+              slide.elements.map(async (element) => {
+                if (
+                  element.type === "img" ||
+                  element.type === "audio" ||
+                  (element.type === "mix" && element.value.file)
+                ) {
+                  const fieldName = `chapter_${chapterIndex}_slide_${slideIndex}_${
+                    element.type === "mix" ? "mix_file" : element.type
+                  }`;
+                  const file = files.find((f) => f.fieldname === fieldName);
+                  if (file) {
+                    return {
+                      ...element,
+                      value:
+                        element.type === "mix"
+                          ? {
+                              ...element.value,
+                              file: file.path,
+                            }
+                          : file.path,
+                    };
+                  }
+                }
+                return element;
+              })
+            ),
+          }))
+        ),
+      }))
+    );
 
-          // If it's an audio type element
-          if (element.type === "audio") {
-            const fieldName = `chapter_${chapterIndex}_slide_${slideIndex}_audio`;
-            const file = req.files.find((f) => f.fieldname === fieldName);
-            if (file) {
-              return {
-                ...element,
-                value: file.filename,
-              };
-            }
-          }
-
-          return element;
-        }),
-      })),
-    }));
-
-    // Update the course properties
     course.title = title || course.title;
     course.description = description || course.description;
     course.published = published || course.published;
     course.chapters =
       updatedChapters.length > 0 ? updatedChapters : course.chapters;
 
-    // Handle the course image separately
     const imageFile = files.find((file) => file.fieldname === "image");
     if (imageFile) {
-      course.image = imageFile.filename;
+      course.image = imageFile.path;
     }
 
-    // Save the updated course
     await course.save();
     res.status(200).json({ message: "Course updated successfully", course });
   } catch (error) {
@@ -291,6 +259,27 @@ courseController.delete("/delete/:id", async (req, res) => {
     if (!course) {
       return res.status(404).json({ message: "Course not found" });
     }
+
+    // Delete associated files from Cloudinary
+    await Promise.all([
+      cloudinary.uploader.destroy(course.image),
+      ...course.chapters.flatMap((chapter) =>
+        chapter.slides.flatMap((slide) =>
+          slide.elements
+            .filter(
+              (element) =>
+                element.type === "img" ||
+                element.type === "audio" ||
+                (element.type === "mix" && element.value.file)
+            )
+            .map((element) =>
+              cloudinary.uploader.destroy(
+                element.type === "mix" ? element.value.file : element.value
+              )
+            )
+        )
+      ),
+    ]);
 
     await Course.deleteOne({ _id: courseId });
     res.status(200).json({ message: "Course deleted successfully" });
